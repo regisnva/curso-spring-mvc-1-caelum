66e875711767ea4a5cfddb1d4746141ba7b6f830
reginaldo.andrade <regisnva@gmail.com>
2016-11-15 12:21:43 -0200

    Atividades Módulo 1 - Começando com SpringMVC

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.casadocodigo.loja.conf;
+
+import br.com.casadocodigo.loja.controller.HomeController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ *
+ * @author regis
+ */
+@EnableWebMvc
+@ComponentScan(basePackageClasses = { HomeController.class })
+public class AppWebConfiguration {
+    
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver(){
+        
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        
+        return resolver;
+    }
+    
+}
---
+++ casadocodigo/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : home
+    Created on : Nov 15, 2016, 11:47:59 AM
+    Author     : regis
+--%>
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Livros de java, Android, Iphone, PHP, Ruby e muito mais - Casa do código</title>
+    </head>
+    <body>
+        <h1>Casa do código</h1>
+    </body>
+</html>
